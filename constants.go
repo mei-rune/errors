cd33141a24752ae9254581324c2a3544d8bb71a7
meifakun <runner.mei@gmail.com>
2019-09-24 10:07:13 +0800

    增加了更多的错误

--- constants.go
+++ constants.go
@@ -7,26 +7,26 @@ import "net/http"
 //)
 
 var (
-	ErrTimeout        = NewHTTPError(http.StatusGatewayTimeout*1000+1, "timeout")
-	ErrNotFound       = NewHTTPError(http.StatusNotFound*1000, "not found")
-	ErrDisabled       = NewHTTPError(http.StatusForbidden*1000+1, "disabled")
-	ErrNotAcceptable  = NewHTTPError(http.StatusNotAcceptable*1000+1, "not acceptable")
-	ErrNotImplemented = NewHTTPError(http.StatusNotImplemented*1000+1, "not implemented ")
-	ErrPending        = NewHTTPError(570*1000+1, "pending")
-	ErrRequired       = NewHTTPError(http.StatusBadRequest*1000+900, "required")
+	ErrTimeout        = NewError(http.StatusGatewayTimeout*1000+1, "timeout")
+	ErrNotFound       = NewError(http.StatusNotFound*1000, "not found")
+	ErrDisabled       = NewError(http.StatusForbidden*1000+1, "disabled")
+	ErrNotAcceptable  = NewError(http.StatusNotAcceptable*1000+1, "not acceptable")
+	ErrNotImplemented = NewError(http.StatusNotImplemented*1000+1, "not implemented ")
+	ErrPending        = NewError(570*1000+1, "pending")
+	ErrRequired       = NewError(http.StatusBadRequest*1000+900, "required")
 
-	ErrNetworkError     = NewHTTPError(560000, "network error")
-	ErrInterruptError   = NewHTTPError(561000, "interrupt error")
-	ErrMultipleError    = NewHTTPError(562000, "multiple error")
-	ErrTableIsNotExists = NewHTTPError(591000, "table isnot exists")
-	ErrResultEmpty      = NewHTTPError(592000, "results is empty")
-	ErrKeyNotFound      = NewHTTPError(http.StatusNotFound*1000+501, "key isnot exists")
+	ErrNetworkError     = NewError(560000, "network error")
+	ErrInterruptError   = NewError(561000, "interrupt error")
+	ErrMultipleError    = NewError(562000, "multiple error")
+	ErrTableIsNotExists = NewError(591000, "table isnot exists")
+	ErrResultEmpty      = NewError(592000, "results is empty")
+	ErrKeyNotFound      = NewError(http.StatusNotFound*1000+501, "key isnot exists")
 
-	ErrReadResponseFail      = NewHTTPError(560011, "network error")
-	ErrUnmarshalResponseFail = NewHTTPError(560012, "network error")
+	ErrReadResponseFail      = NewError(560011, "network error")
+	ErrUnmarshalResponseFail = NewError(560012, "network error")
 
 	ArgumentMissing = ErrRequired
-	ArgumentEmpty   = NewHTTPError(http.StatusBadRequest*1000+901, "empty")
+	ArgumentEmpty   = NewError(http.StatusBadRequest*1000+901, "empty")
 )
 
 func ToHttpCode(code int) int {
