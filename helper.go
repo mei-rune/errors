8ab5f961a15424980e42c5741c85545ccd36e267
meifakun <runner.mei@gmail.com>
2020-12-07 08:01:05 +0800

    add Join

--- helper.go
+++ helper.go
@@ -38,6 +38,16 @@ func NewValidationError(message string) *Error {
 	return &Error{Code: ErrValidationError.ErrorCode(), Message: message}
 }
 
+func Join(err1, err2 error) error {
+	if err1 == nil {
+		return err2
+	}
+	if err2 == nil {
+		return err1
+	}
+	return ErrArray(err1, err2)
+}
+
 func Concat(list ...Error) *Error {
 	return &Error{Code: ErrMultipleError.ErrorCode(), Internals: list}
 }
